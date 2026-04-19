0d8e28469f22cf99ac057e32b4565218be849e6e
Zerty03 <95393542+Zerty03@users.noreply.github.com>
2026-04-19 16:42:33 -0300

    Analisador sintatico

--- Compilador.py
+++ Compilador.py
@@ -151,6 +151,108 @@ class NoComando(NoAST):
         #Ajuda a identificar as palavras IF e WHILE
         self.nome = nome
 
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.pos = 0 #aponta para o token atual
+
+    def token_atual(self):
+        # Retorna o token atual ou um marcador de fim de arquivo
+        if self.pos < len(self.tokens):
+            return self.tokens[self.pos]
+        return ("EOF", "")
+    
+    def avancar(self):
+        #avançar proximo token
+        self.pos += 1
+
+    def erro(self, esperado):
+        #leanta um erro sintático e para a compilação
+        tipo, valor = self.token_atual()
+        raise SyntaxError(f"Erro Sintatico: Esperado '{esperado}', mas encontrou '{valor}' na posição {self.pos + 1} ")
+    
+    def match(self, tipo_esperado, valor_esperado = None):
+        #verifica se o token atual é o esperado
+        tipo, valor = self.token_atual()
+        if tipo == tipo_esperado and (valor_esperado is None or valor == valor_esperado):
+            self.avancar()
+            return valor
+        self.erro(valor_esperado if valor_esperado else tipo_esperado)
+
+    #regra 1: S -> START C END
+    def parse_programa(self):
+        no_prog = NoPrograma()
+        self.match("START", "START")
+        no_prog.comandos = self.parse_comandos()
+        self.match("END", "END")
+        return no_prog
+    
+    #Regra 2: C -> B C | ε
+    def parse_comandos(self):
+        comandos = []
+        tipo, valor = self.token_atual()
+        #Quando encontrar um '(' sabemos que vem um bloco novo
+        while tipo == "OPERADOR" and valor == "(":
+            comandos.append(self.parse_bloco())
+            tipo, valor = self.token_atual()
+        return comandos
+    
+    #Regra 3: B -> ( O )
+    def parse_bloco(self):
+        self.match("OPERADOR", "(")
+        itens = self.parse_conteudo() #Le tudo o que está dentro
+        self.match("OPERADOR", ")")
+
+        if len(itens) >= 3 and isinstance(itens[-1],NoComando):
+            comando_nome = itens[-1].nome
+
+            if comando_nome == "IF":
+                bloco_verdadeiro = itens[-2]
+                condicao = itens[-3]
+                return NoIf(condicao, bloco_verdadeiro)
+            
+            elif comando_nome == "WHILE":
+                bloco_loop = itens[-2]
+                condicao = itens[-3]
+                return NoWhile(condicao, bloco_loop)
+
+        # Se não for IF nem WHILE é um bloco matemático normal    
+        no_bloco = NoBloco()
+        no_bloco.itens = itens
+        return no_bloco
+    
+    #Regra 4 e 5: O -> I O | ε e I -> num | var | op | cmd | B
+    def parse_conteudo(self):
+        itens = []
+        tipo, valor = self.token_atual()
+
+        # Enquanto não encontrar o ), continua a ler os itens
+        while tipo != "EOF" and not (tipo == "OPERADOR" and valor == ")"):
+            if tipo == "OPERADOR" and valor == "(":
+                #Encontrou um ( então é um subbloco
+                itens.append(self.parse_bloco())
+            elif tipo == "NUMERO":
+                itens.append(NoNumero(valor))
+                self.avancar()
+            elif tipo == "VARIAVEL":
+                itens.append(NoVariavel(valor))
+                self.avancar()
+            elif tipo == "OPERADOR":
+                itens.append(NoOperador(valor))
+                self.avancar()
+            elif tipo == "COMANDO":
+                itens.append(NoComando(valor))
+                self.avancar()
+            else:
+                self.erro("Item válido (Número, Variável, Operador ou Bloco)")
+            
+            tipo, valor = self.token_atual()
+        return itens
+    
+    def analisador_sintatico(tokens):
+        parser = Parser(tokens)
+        return parser.parse_programa()
+
 def gerar_assembly(lista_tokens, nome_arquivo_saida):
 
     global contador_ciclos
